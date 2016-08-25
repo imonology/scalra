cf7aa0e03e3f26aa1f034cbd5442e1fd742a821f
Shun-Yun Hu <syhu@imonology.com>
2016-08-25 16:38:27 +0800

    modify: client-side to include only /lib/scalra.js

--- demo/web/config.js
+++ demo/web/config.js
@@ -5,7 +5,16 @@ var mode = 'dev';
 
 // perform action only after page is loaded
 document.addEventListener ("DOMContentLoaded", function () {
+
 	var onConnected = (typeof onConnect === 'function' ? onConnect : function () {console.log('server connected'); });
 	var onSocketEvent = (typeof onSocketEvent === 'function' ? onSocketEvent : undefined);
-	SR.setSocketServer({port: base_port, onEvent: onSocketEvent, onDone: onConnected});
+		
+	SR.init({
+		port: 		base_port,
+		type:		'sockjs',			// can be 'sockjs' 'socketio' 'http'
+		onEvent:	onSocketEvent,
+		onDone:		onConnected
+	});
+	
+	//SR.setSocketServer({port: base_port, onEvent: onSocketEvent, onDone: onConnected});
 });
