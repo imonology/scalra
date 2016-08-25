a7b9d9f56f15e144f2fb748939c107e6e7bc0e25
Shun-Yun Hu <syhu@imonology.com>
2016-08-25 18:04:05 +0800

    modify: change default sendEvent mode to "http" instead of "sockjs" except chat demo

--- demo/web/config.js
+++ demo/web/config.js
@@ -8,10 +8,13 @@ document.addEventListener ("DOMContentLoaded", function () {
 
 	var onConnected = (typeof onConnect === 'function' ? onConnect : function () {console.log('server connected'); });
 	var onSocketEvent = (typeof onSocketEvent === 'function' ? onSocketEvent : undefined);
-		
+	var type = (typeof connectType === 'string' ? connectType : 'http');
+	
+	console.log('connectType: ' + type);
+	
 	SR.init({
 		port: 		base_port,
-		type:		'sockjs',			// can be 'sockjs' 'socketio' 'http'
+		type:		type,			// can be 'sockjs' 'socketio' 'http'
 		onEvent:	onSocketEvent,
 		onDone:		onConnected
 	});
