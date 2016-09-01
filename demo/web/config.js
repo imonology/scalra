4b090bd3206c17dab486c6ae43afad925bb1ecba
Shun-Yun Hu <syhu@imonology.com>
2016-09-01 11:39:03 +0800

    modify: move /lib/scalra.js init code from project-specific to common

--- demo/web/config.js
+++ demo/web/config.js
@@ -1,23 +1,3 @@
 
 // set base port
-var base_port = 37070;
-var mode = 'dev';
-
-// perform action only after page is loaded
-document.addEventListener ("DOMContentLoaded", function () {
-
-	var onConnected = (typeof onConnect === 'function' ? onConnect : function () {console.log('server connected'); });
-	var onSocketEvent = (typeof onSocketEvent === 'function' ? onSocketEvent : undefined);
-	var type = (typeof connectType === 'string' ? connectType : 'http');
-	
-	console.log('connectType: ' + type);
-	
-	SR.init({
-		port: 		base_port,
-		type:		type,			// can be 'sockjs' 'socketio' 'http'
-		onEvent:	onSocketEvent,
-		onDone:		onConnected
-	});
-	
-	//SR.setSocketServer({port: base_port, onEvent: onSocketEvent, onDone: onConnected});
-});
+var basePort = 37070;
