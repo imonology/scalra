ccfcaf49970b39abbdefaee65455b9425beefa48
ImonCloud <dev@imoncloud.com>
2017-03-22 16:08:33 +0800

    modify: build default /config.js file from /config.js.default if not exist

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -9,11 +9,22 @@ fs.stat('lib/swagger-ui', function (err) {
 				console.error(err);
 			} else {
 				
-				// use minifier version
-				
+				// use minifier version				
 				console.log("swagger-ui ready!");
 			}
 		}); //copies directory, even if it has subdirectories or files		
 	}
 });
 
+fs.stat('config.js', function (err) {
+	if (err) {
+		console.log('copying default config.js file from config.js.default...');
+		fs.copy('config.js.default', 'config.js', function (err) {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log("default config.js ready!");
+			}
+		});
+	}
+});
