ea170d40d970cfb7332172bb7a7716c06510e7b5
ImonCloud <ImonCloud>
2017-05-06 05:20:08 +0800

    syhu/scalra: rename: npm "node-fs-extra" to "fs-extra"

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,6 @@
 // allow access to swagger-ui from /lib
 // ref: http://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
-const fs = require('node-fs-extra');
+const fs = require('fs-extra');
 fs.stat('lib/swagger-ui', function (err) {
 	if (err) {
 		console.log('copying swagger-ui to /lib...');
