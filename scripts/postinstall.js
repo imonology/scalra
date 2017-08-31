62f528795a4b17521018780929e42d88a12ec76e
ImonCloud <dev@imoncloud.com>
2017-08-31 17:00:21 +0800

    syhu/scalra: fix: /scripts/postinstall.js to copy 'swagger-ui' from directory below

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,18 +1,40 @@
 // allow access to swagger-ui from /lib
 // ref: http://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
 const fs = require('fs-extra');
+const path = require('path');
+
+var copy_swagger = function (swagger_path) {
+	fs.copy(path.join(swagger_path, 'dist'), 'lib/swagger-ui', function (err) {
+		if (err) {
+			console.error(err);
+		} else {
+
+			// use minifier version				
+			console.log("swagger-ui ready!");
+		}
+	}); //copies directory, even if it has subdirectories or files	
+}
+
 fs.stat('lib/swagger-ui', function (err) {
 	if (err) {
 		console.log('copying swagger-ui to /lib...');
-		fs.copy('node_modules/swagger-ui/dist', 'lib/swagger-ui', function (err) {
-			if (err) {
-				console.error(err);
-			} else {
-				
-				// use minifier version				
-				console.log("swagger-ui ready!");
+		
+		// check for swagger library
+		var swagger_path = 'node_modules/swagger-ui';
+		fs.stat(swagger_path, function (err) {
+			if (!err) {
+				return copy_swagger(swagger_path);
 			}
-		}); //copies directory, even if it has subdirectories or files		
+	
+			// check elsewhere
+			swagger_path = '../swagger-ui';
+			fs.stat(swagger_path, function (err) {
+				if (err) {
+					return console.error('cannot find swagger-ui in node_modules!');
+				}
+				copy_swagger(swagger_path);
+			})
+		})
 	}
 });
 
