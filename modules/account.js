95ba659d37a7cdf891b79552f4af9d1c434bd173
YY <z3388638@gmail.com>
2018-05-29 15:53:21 +0800

    fix typo

--- modules/account.js
+++ modules/account.js
@@ -254,7 +254,7 @@ SR.API.add('_ACCOUNT_REGISTER', {
 		};
 		
 		// special handling (by default 'admin' account is special and will be part of the 'admin' group by default
-		if (!args.authWP && reg.account === 'admin') {q
+		if (!args.authWP && reg.account === 'admin') {
 			reg.control.groups.push('admin');
 		}
 				
