0f999aaeae437df1f8c61e1333bf98fc9e28dd96
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2021-01-19 15:24:37 +0800

    adding checkers and helpers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,80 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'node': true,
-  },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 2017,
-    'sourceType': 'module',
-  },
-  'globals': {
-    'UTIL': {},
-    'SR': {},
-    'LOG': {},
-  },
-  'rules': {
-    'indent': [
-      'error',
-      'tab',
-      {'SwitchCase': 0},
-    ],
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'always',
-    ],
-    'no-console': [
-      'warn',
-      {allow: ['warn', 'error', 'info']},
-    ],
-    'brace-style': [
-      'error',
-      '1tbs',
-      {'allowSingleLine': true},
-    ],
-    'no-unused-vars': [
-      'warn',
-      {
-        'vars': 'all',
-        'args': 'after-used',
-        'ignoreRestSiblings': false,
-      },
-    ],
-    'no-mixed-spaces-and-tabs': [
-      'error',
-      'smart-tabs',
-    ],
-  },
+	env: {
+		"es6": true,
+		// "es2020": true,
+		"node": true,
+		"browser": true,
+		"shared-node-browser": true,
+		"mocha": true,
+		"jest": true,
+		"mongo": true,
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 11,
+		sourceType: "module",
+	},
+	parser: "babel-eslint",
+	rules: {
+		"indent": [
+			"error",
+			"tab",
+			{
+				VariableDeclarator: "first",
+			},
+		],
+		"linebreak-style": ["error", "unix"],
+		"curly": "error",
+		"quotes": [
+			"warn",
+			"single",
+			{
+				avoidEscape: true,
+				allowTemplateLiterals: true,
+			},
+		],
+		"semi": ["error", "always"],
+		"no-console": [
+			"warn",
+			{
+				allow: ["warn", "error", "info"],
+			},
+		],
+		"brace-style": [
+			"error",
+			"1tbs",
+			{
+				allowSingleLine: true,
+			},
+		],
+		"no-unused-vars": [
+			"warn",
+			{
+				vars: "all",
+				args: "after-used",
+				ignoreRestSiblings: false,
+			},
+		],
+		"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+		"no-trailing-spaces": "error",
+		"arrow-parens": ["error", "always"],
+		"arrow-body-style": ["warn", "always"],
+		"func-style": [
+			"warn",
+			"declaration",
+			{
+				allowArrowFunctions: true,
+			},
+		],
+		"no-confusing-arrow": [
+			"error",
+			{
+				allowParens: false,
+			},
+		],
+		"arrow-spacing": "error",
+		"prefer-arrow-callback": "warn",
+		"require-await": "error",
+		"operator-linebreak": ["warn", "before"],
+	},
 };
